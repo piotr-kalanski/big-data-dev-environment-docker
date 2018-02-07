c1f3bb0b9055c343aaa10f033f9bb5e68121a6e5
piotr-kalanski <piotr.kalanski@gmail.com>
2018-02-07 17:28:56 +0100

    Fixing postgres create users script

--- postgres_users/create_users.sh
+++ postgres_users/create_users.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -e
-
 psql --username "$POSTGRES_USER" -c "CREATE USER airflow WITH PASSWORD 'airflow';"
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE airflow;
     GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
